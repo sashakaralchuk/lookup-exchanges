200210c038b8f95a9253f8401a75848bd4fda012
Sasha Karalchuk <sasha.karalchuk@gmail.com>
2025-02-17 01:06:30 +0100

    chore: adjust lookups

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,76 @@
 use std::collections::HashMap;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     dotenv::dotenv().ok();
     let config = Config::new_from_envs();
-    let client_clickhouse = clickhouse::Client::default().with_url(config.clickhouse_url);
-    let (tx, rx) = std::sync::mpsc::channel::<Kline>();
-    let mut curr_klines = {
-        let mut m_symbols = HashMap::new();
-        for symbol in config.poloniex_symbols.clone() {
-            let mut m_timeframes: HashMap<KlineTimeframe, std::option::Option<Kline>> =
-                HashMap::new();
-            for timeframe in config.poloniex_timeframes.clone() {
-                m_timeframes.insert(timeframe, None);
-            }
-            m_symbols.insert(symbol, m_timeframes);
+    let config_fill_absent_kline = config.clone();
+    let client_clickhouse = clickhouse::Client::default().with_url(&config.clickhouse_url);
+    let client_clickhouse_curr_kline = client_clickhouse.clone();
+    let client_clickhouse_fill_absent_kline = client_clickhouse.clone();
+    let (tx_curr_kline, rx_curr_kline) = std::sync::mpsc::channel();
+    let (tx_fill_absent_kline, rx_fill_absent_kline) = std::sync::mpsc::channel();
+    let mut curr_klines = HashMap::new();
+    let mut first_trade_ts = HashMap::new();
+    for symbol in config.poloniex_symbols.clone() {
+        let mut m_timeframes: HashMap<KlineTimeframe, std::option::Option<Kline>> = HashMap::new();
+        let mut m_timeframes_ts: HashMap<KlineTimeframe, std::option::Option<i64>> = HashMap::new();
+        for timeframe in config.poloniex_timeframes.clone() {
+            m_timeframes.insert(timeframe.clone(), None);
+            m_timeframes_ts.insert(timeframe.clone(), None);
+            poloniex_pub::fetch_insert_klines(
+                &client_clickhouse,
+                config.start_date_millis,
+                &symbol,
+                &timeframe,
+            )
+            .await?;
         }
-        m_symbols
-    };
-    let on_message = |event: poloniex_ws_pub::Event| {
-        if let poloniex_ws_pub::Event::Trade(t) = event {
+        curr_klines.insert(symbol.clone(), m_timeframes);
+        first_trade_ts.insert(symbol, m_timeframes_ts);
+    }
+    let on_message = |event: poloniex_pub::EventWs| {
+        if let poloniex_pub::EventWs::Trade(t) = event {
             for trade in t.conv_to_recent_trade_vec() {
                 log::debug!("trade={:?}", trade);
                 let curr_timeframes = curr_klines.get_mut(&trade.pair).unwrap();
                 for (timeframe, curr_kline) in curr_timeframes.iter_mut() {
+                    let first_trade_ts = first_trade_ts
+                        .get_mut(&trade.pair)
+                        .unwrap()
+                        .get_mut(timeframe)
+                        .unwrap();
+                    if first_trade_ts.is_none() {
+                        log::debug!(
+                            "fill first_trade_ts for ({}, {:?}) to miss constructing inconsistent kline",
+                            trade.pair,
+                            timeframe.to_str()
+                        );
+                        *first_trade_ts = Some(trade.timestamp);
+                        continue;
+                    }
+                    let first_trade_ts_threshold = first_trade_ts.unwrap()
+                        / timeframe.to_inserval_millis()
+                        * timeframe.to_inserval_millis()
+                        + timeframe.to_inserval_millis()
+                        - 1;
+                    if trade.timestamp <= first_trade_ts_threshold {
+                        log::debug!("skip trade={:?} due to avoid inconsistent kline", trade);
+                        continue;
+                    }
                     if curr_kline.is_none() {
+                        log::debug!("start constructing new kline");
                         curr_kline.replace(Kline::new_from_trade(timeframe.clone(), &trade));
+                        tx_fill_absent_kline
+                            .send((trade.pair.clone(), timeframe.clone()))
+                            .unwrap();
                         continue;
                     }
                     if curr_kline.as_mut().unwrap().expired(&trade) {
-                        tx.send(curr_kline.as_mut().unwrap().clone()).unwrap();
+                        tx_curr_kline
+                            .send(curr_kline.as_mut().unwrap().clone())
+                            .unwrap();
                         curr_kline.replace(Kline::new_from_trade(timeframe.clone(), &trade));
                         continue;
                     }
@@ -40,28 +80,36 @@ async fn main() {
         }
     };
     tokio::spawn(async move {
-        loop {
-            match rx.recv_timeout(std::time::Duration::from_millis(100)) {
-                Ok(k) => {
-                    log::info!("save kline to db k={:?}", k);
-                    let mut insert_klines = client_clickhouse.insert("klines").unwrap();
-                    insert_klines.write(&k.to_row()).await.unwrap();
-                    insert_klines.end().await.unwrap();
-                }
-                Err(e) => log::debug!("try_recv error {:?}", e),
-            }
+        while let Ok(k) = rx_curr_kline.recv() {
+            log::info!("save kline to db k={:?}", k);
+            let mut insert_klines = client_clickhouse_curr_kline.insert("klines").unwrap();
+            insert_klines.write(&k.to_row()).await.unwrap();
+            insert_klines.end().await.unwrap();
         }
     });
-    poloniex_ws_pub::listen_pub_channel(
-        &poloniex_ws_pub::Config::new(poloniex_ws_pub::Channel::Trade, config.poloniex_symbols),
+    tokio::spawn(async move {
+        while let Ok((pair, timeframe)) = rx_fill_absent_kline.recv() {
+            log::info!("fill absent kline in db pair={pair} timeframe={timeframe:?}");
+            poloniex_pub::fetch_insert_klines(
+                &client_clickhouse_fill_absent_kline,
+                config_fill_absent_kline.start_date_millis,
+                &pair,
+                &timeframe,
+            )
+            .await
+            .unwrap();
+        }
+    });
+    poloniex_pub::listen_ws_channel(
+        &poloniex_pub::ConfigWs::new(poloniex_pub::ChannelWs::Trade, config.poloniex_symbols),
         on_message,
     )
-    .await
-    .unwrap();
+    .await?;
+    Ok(())
 }
 
-mod poloniex_ws_pub {
-    use crate::{now_millis, RecentTrade};
+mod poloniex_pub {
+    use crate::{millis_to_hr_str, now_millis, Kline, KlineTimeframe, RecentTrade, VBS};
     use async_tungstenite::async_std::connect_async;
     use async_tungstenite::tungstenite::Message;
     use futures::StreamExt;
@@ -70,13 +118,13 @@ mod poloniex_ws_pub {
 
     const URL_WS: &str = "wss://ws.poloniex.com/ws/public";
 
-    pub async fn listen_pub_channel(
-        config: &Config,
-        on_message: impl FnMut(Event),
+    pub async fn listen_ws_channel(
+        config: &ConfigWs,
+        on_message: impl FnMut(EventWs),
     ) -> Result<(), Box<dyn std::error::Error>> {
         let (mut socket, _) = connect_async(URL_WS).await?;
         let channel_str = match config.channel {
-            Channel::Trade => "trades",
+            ChannelWs::Trade => "trades",
         };
         let sumbols_str = config
             .symbols
@@ -107,61 +155,160 @@ mod poloniex_ws_pub {
             };
             let msg_str = msg.to_text().unwrap();
             log::debug!("msg_str={msg_str}");
-            if let Ok(_) = serde_json::from_str::<EventPingRaw>(msg_str) {
+            if let Ok(_) = serde_json::from_str::<EventWsPingRaw>(msg_str) {
                 continue;
             }
-            if let Ok(o) = serde_json::from_str::<EventSubscribeRaw>(msg_str) {
-                f(Event::Subscribe(o));
+            if let Ok(o) = serde_json::from_str::<EventWsSubscribeRaw>(msg_str) {
+                f(EventWs::Subscribe(o));
                 continue;
             }
-            if let Ok(o) = serde_json::from_str::<EventTradeRaw>(msg_str) {
-                f(Event::Trade(o));
+            if let Ok(o) = serde_json::from_str::<EventWsTradeRaw>(msg_str) {
+                f(EventWs::Trade(o));
                 continue;
             }
             panic!("unknown msg_str={msg_str}");
         }
     }
 
-    pub struct Config {
-        channel: Channel,
+    /// Fetches all klines except current.
+    pub async fn fetch_klines(
+        symbol: &str,
+        timeframe: KlineTimeframe,
+        start_millis: i64,
+        end_millis: i64,
+    ) -> Result<Vec<Kline>, Box<dyn std::error::Error>> {
+        let interval_str = match timeframe {
+            KlineTimeframe::Minute(1) => "MINUTE_1".to_string(),
+            KlineTimeframe::Minute(15) => "MINUTE_15".to_string(),
+            KlineTimeframe::Hour(1) => "HOUR_1".to_string(),
+            KlineTimeframe::Day(1) => "DAY_1".to_string(),
+            _ => panic!("inproper timeframe={timeframe:?}"),
+        };
+        let limit = 500;
+        let interval_millis = timeframe.to_inserval_millis();
+        let window_millis = interval_millis * (limit - 1);
+        let end_adj_millis = end_millis / interval_millis * interval_millis - 1;
+        log::debug!(
+            "start_ts={} end_adj_millis={}",
+            millis_to_hr_str(start_millis),
+            millis_to_hr_str(end_adj_millis)
+        );
+        let mut klines = vec![];
+        for i in 0..((end_millis - start_millis) / window_millis + 1) {
+            let start_time = start_millis + i * window_millis;
+            let end_time = i64::min(start_millis + (i + 1) * window_millis - 1, end_adj_millis);
+            let url = format!(
+                "https://api.poloniex.com/markets/{}/candles?interval={}&startTime={}&endTime={}&limit={}",
+                symbol,
+                interval_str,
+                start_time,
+                end_time,
+                limit
+            );
+            log::debug!("url={url}");
+            let res = reqwest::Client::new()
+                .get(url)
+                .header("Content-Type", "application/json")
+                .send()
+                .await?;
+            let res_text = res.text().await.unwrap();
+            let mut res_obj: Vec<EventHttpKlineRaw> = serde_json::from_str(&res_text).unwrap();
+            res_obj.sort_by(|a, b| a.12.partial_cmp(&b.12).unwrap());
+            klines.extend(
+                res_obj
+                    .iter()
+                    .map(|o| o.to_kline(&symbol.to_string()))
+                    .into_iter(),
+            );
+            log::info!("res_obj={:?}", res_obj.len());
+        }
+        log::info!("klines.len()={}", klines.len());
+        return Ok(klines);
+    }
+
+    pub async fn fetch_insert_klines(
+        client_clickhouse: &clickhouse::Client,
+        start_date_millis: i64,
+        symbol: &String,
+        timeframe: &KlineTimeframe,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let end_millis = now_millis();
+        log::info!("load klines symbol={} timeframe={:?}", symbol, timeframe);
+        let last_kline_utc_begin = client_clickhouse
+            .query(
+                r#"
+                SELECT toUnixTimestamp(utc_begin)
+                FROM default.klines
+                WHERE pair = ? AND time_frame = ?
+                ORDER BY utc_begin DESC
+                LIMIT 1
+                "#,
+            )
+            .bind(&symbol)
+            .bind(&timeframe.to_str())
+            .fetch::<i32>()
+            .unwrap()
+            .next()
+            .await?;
+        let start_millis = match last_kline_utc_begin {
+            Some(ts) => (ts as i64) * 1000 + timeframe.to_inserval_millis(),
+            None => start_date_millis,
+        };
+        let klines = fetch_klines(symbol, timeframe.clone(), start_millis, end_millis).await?;
+        log::info!(
+            "save klines to db symbol={} timeframe={:?} klines.len()={}",
+            symbol,
+            timeframe,
+            klines.len()
+        );
+        let mut insert_klines = client_clickhouse.insert("klines")?;
+        for k in klines.iter() {
+            insert_klines.write(&k.to_row()).await?;
+        }
+        insert_klines.end().await?;
+        Ok(())
+    }
+
+    pub struct ConfigWs {
+        channel: ChannelWs,
         symbols: Vec<String>,
     }
 
-    impl Config {
-        pub fn new(channel: Channel, symbols: Vec<String>) -> Self {
+    impl ConfigWs {
+        pub fn new(channel: ChannelWs, symbols: Vec<String>) -> Self {
             Self { channel, symbols }
         }
     }
 
-    pub enum Channel {
+    pub enum ChannelWs {
         Trade,
     }
 
-    pub enum Event {
-        Subscribe(EventSubscribeRaw),
-        Trade(EventTradeRaw),
+    pub enum EventWs {
+        Subscribe(EventWsSubscribeRaw),
+        Trade(EventWsTradeRaw),
     }
 
     #[derive(serde::Deserialize)]
-    pub struct EventSubscribeRaw {
+    pub struct EventWsSubscribeRaw {
         pub event: String,
         pub channel: String,
         pub symbols: Vec<String>,
     }
 
     #[derive(serde::Deserialize)]
-    pub struct EventPingRaw {
+    pub struct EventWsPingRaw {
         pub event: String,
     }
 
     #[derive(serde::Deserialize)]
-    pub struct EventTradeRaw {
+    pub struct EventWsTradeRaw {
         pub channel: String,
-        pub data: Vec<EventTradeRawData>,
+        pub data: Vec<EventWsTradeRawData>,
     }
 
     #[derive(serde::Deserialize)]
-    pub struct EventTradeRawData {
+    pub struct EventWsTradeRawData {
         symbol: String,
         amount: String,
         quantity: String,
@@ -174,7 +321,7 @@ mod poloniex_ws_pub {
         ts: i64,
     }
 
-    impl EventTradeRaw {
+    impl EventWsTradeRaw {
         pub fn conv_to_recent_trade_vec(&self) -> Vec<RecentTrade> {
             self.data
                 .iter()
@@ -187,6 +334,52 @@ mod poloniex_ws_pub {
                     timestamp: d.createTime,
                 })
                 .collect()
+        }
+    }
+
+    #[derive(serde::Deserialize, Debug)]
+    pub struct EventHttpKlineRaw(
+        #[serde(rename = "low")] String,
+        #[serde(rename = "high")] String,
+        #[serde(rename = "open")] String,
+        #[serde(rename = "close")] String,
+        #[serde(rename = "amount")] String,
+        #[serde(rename = "quantity")] String,
+        #[serde(rename = "buyTakerAmount")] String,
+        #[serde(rename = "buyTakerQuantity")] String,
+        #[serde(rename = "tradeCount")] i64,
+        #[serde(rename = "ts")] i64,
+        #[serde(rename = "weightedAverage")] String,
+        #[serde(rename = "interval")] String,
+        #[serde(rename = "startTime")] i64,
+        #[serde(rename = "closeTime")] i64,
+    );
+
+    impl EventHttpKlineRaw {
+        fn to_kline(&self, symbol: &String) -> Kline {
+            let time_frame = match self.11.as_str() {
+                "MINUTE_1" => KlineTimeframe::Minute(1),
+                "MINUTE_15" => KlineTimeframe::Minute(15),
+                "HOUR_1" => KlineTimeframe::Hour(1),
+                "DAY_1" => KlineTimeframe::Day(1),
+                _ => panic!("unknown interval={}", self.11),
+            };
+            Kline {
+                time_frame,
+                pair: symbol.clone(),
+                o: self.2.parse().unwrap(),
+                h: self.1.parse().unwrap(),
+                l: self.0.parse().unwrap(),
+                c: self.3.parse().unwrap(),
+                utc_begin: self.12,
+                volume_bs: VBS {
+                    buy_base: self.7.parse().unwrap(),
+                    sell_base: self.5.parse().unwrap(),
+                    buy_quote: self.6.parse().unwrap(),
+                    sell_quote: self.4.parse().unwrap(),
+                },
+                last_tid: 0,
+            }
         }
     }
 }
@@ -345,10 +538,18 @@ fn now_millis() -> i64 {
     chrono::Utc::now().timestamp_millis()
 }
 
+fn millis_to_hr_str(m: i64) -> String {
+    chrono::DateTime::from_timestamp_millis(m)
+        .unwrap()
+        .to_string()
+}
+
+#[derive(Clone)]
 struct Config {
-    pub clickhouse_url: String,
-    pub poloniex_symbols: Vec<String>,
-    pub poloniex_timeframes: Vec<KlineTimeframe>,
+    clickhouse_url: String,
+    poloniex_symbols: Vec<String>,
+    poloniex_timeframes: Vec<KlineTimeframe>,
+    start_date_millis: i64,
 }
 
 impl Config {
@@ -367,6 +568,15 @@ impl Config {
                 .into_iter()
                 .map(|s| KlineTimeframe::new_from_str(s))
                 .collect::<Vec<KlineTimeframe>>(),
+            start_date_millis: chrono::NaiveDate::parse_from_str(
+                &std::env::var("START_DATE").unwrap(),
+                "%Y-%m-%d",
+            )
+            .unwrap()
+            .and_hms_opt(0, 0, 0)
+            .unwrap()
+            .and_utc()
+            .timestamp_millis(),
         }
     }
 }
